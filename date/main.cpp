9b34da07c6497a2ad40fad81b684b9bf3066c06a
Robert Krahn <r@packetcloud.de>
2025-05-06 12:11:32 +0200

    Datumsklasse

---
+++ date/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class date
+{
+public:
+    void init(int, int, int);
+    void print();
+
+private:
+    int day_;
+    int month_;
+    int year_;
+};
+
+void date::init(int d, int m, int y)
+{
+    day_ = d;
+    month_ = m;
+    year_ = y;
+}
+void date::print()
+{
+    std::cout << day_ << '.' << month_ << '.' << year_ << std::endl;
+}
+
+int main()
+{
+    date d1;               // ein Datums-Objekt wird erzeugt
+    date d2, d3;           // zwei Datums-Objekte werden erzeugt
+    d1.init(29, 11, 2004); // d1 wird mit dem 29.11.2004 initialisiert
+    d2.init(10, 5, 1999);  // d2 wird mit dem 10.05.1999 initialisiert
+    d3.init(5, 1, 2005);   // d3 wird mit dem 05.01.2005 initialisiert
+    d1.print();            // => 29.11.2004
+    d2.print();            // => 10.05.1999
+    d3.print();            // => 05.01.2005
+}
